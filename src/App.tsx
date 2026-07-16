ff8645f71ad2b457387c223d42b56202444abd81
Cícero Thyago <c.thyago.of@gmail.com>
2026-07-15 23:57:18 -0300

    fix: restaura basename do BrowserRouter perdido em commit anterior

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { CropPage } from './pages/CropPage';
 import { ImageToPdfPage } from './pages/ImageToPdfPage';
 import { MergePdfPage } from './pages/MergePdfPage';
 import { SplitPdfPage } from './pages/SplitPdfPage';
-import { PdfToImagePage } from './pages/PdfToImagePage'; // Importação
+import { PdfToImagePage } from './pages/PdfToImagePage';
 import { BgRemovePage } from './pages/BgRemovePage';
 import { useSettingsStore } from './store/useSettingsStore';
 import { useHistoryStore } from './store/useHistoryStore';
@@ -32,7 +32,7 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="min-h-screen flex flex-col relative bg-background">
         <Header />
         
@@ -45,7 +45,7 @@ export default function App() {
           <Route path="/pdf" element={<ImageToPdfPage />} />
           <Route path="/merge-pdf" element={<MergePdfPage />} />
           <Route path="/split-pdf" element={<SplitPdfPage />} />
-          <Route path="/pdf-to-image" element={<PdfToImagePage />} /> {/* Nova Rota */}
+          <Route path="/pdf-to-image" element={<PdfToImagePage />} />
           <Route path="/bg-remove" element={<BgRemovePage />} />
           <Route path="/saved" element={<SavedPage />} />
           <Route path="/profile" element={<ProfilePage />} />
